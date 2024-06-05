d14eb96a7b68f21cf53db486c075db72ae8de5e9
Ranga Dharma Teja Kuntumalla <rangadharmateja0@gmail.com>
2024-06-05 10:29:24 +0530

    1- update the routes and server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from "express";
 
-import authRoutes from './routes/auth.routes.js';
-import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
 
 import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from 'cloudinary'
-import postRoutes from './routes/post.routes.js'
+import postRoutes from './routes/post.route.js'
 
 dotenv.config();
 
