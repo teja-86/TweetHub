a886bc3615dbe500be47a7cd2ef56fe3f950fa5c
Ranga Dharma Teja Kuntumalla <rangadharmateja0@gmail.com>
2024-06-08 18:17:55 +0530

    3 - profile page posts and user info fetched!

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -2,7 +2,7 @@ import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import {useQuery} from '@tanstack/react-query'
 import { useEffect } from "react";
-const Posts = ({feedType}) => {
+const Posts = ({feedType, username, userId}) => {
 
 	const getPostEndpoint = () => {
 		switch(feedType) {
@@ -11,6 +11,10 @@ const Posts = ({feedType}) => {
 				return "/api/posts/all";
 			case "following":
 				return "/api/posts/following";
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+				return `/api/posts/likes/${userId}`
 			default:
 				return "/api/posts/all";
 		}
@@ -36,7 +40,7 @@ const Posts = ({feedType}) => {
 
 	useEffect(() => {
 		refetch();
-	}, [feedType, refetch]);
+	}, [feedType, refetch, username]);
 
 	return (
 		<>
